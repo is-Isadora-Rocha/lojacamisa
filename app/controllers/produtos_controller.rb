4670a55797be21b88cfceb7b96ccf4688d16f379
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-01-16 15:44:38 -0300

    comentario de limite de preõ

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -3,6 +3,8 @@ class ProdutosController < ApplicationController
   def index
     @produtos_nome = Produto.order :nome
     @produtos_preco = Produto.order :preco
+    #@produtos_preco = Produto.order(:preco).limit 3
+    #limitando para 3 preços mais baratos
   end
 
 end
