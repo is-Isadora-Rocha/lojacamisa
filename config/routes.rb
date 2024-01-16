6fd90f0b1103c1cf2843735b7e6db4229453e716
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-01-16 11:12:38 -0300

    primeiro controller

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "teste" => "teste#index"
+  #get "nome_url_desejada" =>
+  #get "up" => "rails/health#show", as: :rails_health_check
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
